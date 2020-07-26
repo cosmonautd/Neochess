a71cb5f4d62de07ff880d3105befc8b41729a001
David Borges <davidborges@protonmail.com>
2020-07-25 22:54:29 -0300

    New drag and drop package

--- neochess-web/src/components/NeoBoard.vue
+++ neochess-web/src/components/NeoBoard.vue
@@ -1,148 +1,16 @@
 <template>
 <div id="neochess-board">
-	<div class="board" :style="{width: size + 'px', height: size + 'px'}">
-		<div class="rank">
-			<div class="light-square">
-				<NeoPiece id="b_rook" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="b_knight" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="b_bishop" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="b_queen" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="b_king" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="b_bishop" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="b_knight" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="b_rook" :size="piece_size"/>
-			</div>
-		</div>
-		<div class="rank">
-			<div class="dark-square">
-				<NeoPiece id="b_pawn" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="b_pawn" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="b_pawn" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="b_pawn" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="b_pawn" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="b_pawn" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="b_pawn" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="b_pawn" :size="piece_size"/>
-			</div>
-		</div>
-		<div class="rank">
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-		</div>
-		<div class="rank">
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-		</div>
-		<div class="rank">
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-		</div>
-		<div class="rank">
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-			<div class="dark-square"></div>
-			<div class="light-square"></div>
-		</div>
-		<div class="rank">
-			<div class="light-square">
-				<NeoPiece id="w_pawn" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="w_pawn" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="w_pawn" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="w_pawn" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="w_pawn" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="w_pawn" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="w_pawn" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="w_pawn" :size="piece_size"/>
-			</div>
-		</div>
-		<div class="rank">
-			<div class="dark-square">
-				<NeoPiece id="w_rook" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="w_knight" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="w_bishop" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="w_queen" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="w_king" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="w_bishop" :size="piece_size"/>
-			</div>
-			<div class="dark-square">
-				<NeoPiece id="w_knight" :size="piece_size"/>
-			</div>
-			<div class="light-square">
-				<NeoPiece id="w_rook" :size="piece_size"/>
+	<div class="board" :style="{width: size + 'px', height: size + 'px'}" :key="board_update">
+		<div v-for="file in board" :key="file.id">
+			<div v-for="rank in file.ranks" :key="rank.square" class="rank">
+				<Drop v-if="rank.piece" :class="`drop ${rank.type}`" drop-effect="move"
+					@drop="(data, e) => move({...data, ...{square: rank.square}})">
+					<NeoPiece :id="rank.piece" :start_position="rank.square" :size="piece_size"/>
+				</Drop>
+				<Drop v-else  :class="`drop ${rank.type}`" drop-effect="move"
+					@drop="(data, e) => move({...data, ...{square: rank.square}})">
+					<div :style="{width: piece_size + 'px', height: piece_size + 'px'}"/>
+				</Drop>
 			</div>
 		</div>
 	</div>
@@ -151,10 +19,12 @@
 
 <script>
 import NeoPiece from "./NeoPiece.vue";
+import { Drop } from 'vue-drag-drop';
 export default {
 	name: "neochess-board",
 	components: {
-		NeoPiece
+		NeoPiece,
+		Drop
 	},
 	props: {
 		size: {
@@ -165,12 +35,126 @@ export default {
 	data() {
 		return {
 			game_id: this.$route.params.game_id,
-			piece_size: this.size*0.125
+			piece_size: this.size*0.125,
+			board_update: true,
+			board: [
+				{
+					id: 'h',
+					ranks: [
+						{ square: 'h8', type: 'light-square', piece: 'b_rook' },
+						{ square: 'h7', type: 'dark-square', piece: 'b_knight' },
+						{ square: 'h6', type: 'light-square', piece: 'b_bishop' },
+						{ square: 'h5', type: 'dark-square', piece: 'b_queen' },
+						{ square: 'h4', type: 'light-square', piece: 'b_king' },
+						{ square: 'h3', type: 'dark-square', piece: 'b_bishop' },
+						{ square: 'h2', type: 'light-square', piece: 'b_knight' },
+						{ square: 'h1', type: 'dark-square', piece: 'b_rook' },
+					]
+				},
+				{
+					id: 'g',
+					ranks: [
+						{ square: 'g8', type: 'dark-square', piece: 'b_pawn' },
+						{ square: 'g7', type: 'light-square', piece: 'b_pawn' },
+						{ square: 'g6', type: 'dark-square', piece: 'b_pawn' },
+						{ square: 'g5', type: 'light-square', piece: 'b_pawn' },
+						{ square: 'g4', type: 'dark-square', piece: 'b_pawn' },
+						{ square: 'g3', type: 'light-square', piece: 'b_pawn' },
+						{ square: 'g2', type: 'dark-square', piece: 'b_pawn' },
+						{ square: 'g1', type: 'light-square', piece: 'b_pawn' },
+					]
+				},
+				{
+					id: 'f',
+					ranks: [
+						{ square: 'f8', type: 'light-square', piece: null },
+						{ square: 'f7', type: 'dark-square', piece: null },
+						{ square: 'f6', type: 'light-square', piece: null },
+						{ square: 'f5', type: 'dark-square', piece: null },
+						{ square: 'f4', type: 'light-square', piece: null },
+						{ square: 'f3', type: 'dark-square', piece: null },
+						{ square: 'f2', type: 'light-square', piece: null },
+						{ square: 'f1', type: 'dark-square', piece: null },
+					]
+				},
+				{
+					id: 'e',
+					ranks: [
+						{ square: 'e8', type: 'dark-square', piece: null },
+						{ square: 'e7', type: 'light-square', piece: null },
+						{ square: 'e6', type: 'dark-square', piece: null },
+						{ square: 'e5', type: 'light-square', piece: null },
+						{ square: 'e4', type: 'dark-square', piece: null },
+						{ square: 'e3', type: 'light-square', piece: null },
+						{ square: 'e2', type: 'dark-square', piece: null },
+						{ square: 'e1', type: 'light-square', piece: null },
+					]
+				},
+				{
+					id: 'd',
+					ranks: [
+						{ square: 'd8', type: 'light-square', piece: null },
+						{ square: 'd7', type: 'dark-square', piece: null },
+						{ square: 'd6', type: 'light-square', piece: null },
+						{ square: 'd5', type: 'dark-square', piece: null },
+						{ square: 'd4', type: 'light-square', piece: null },
+						{ square: 'd3', type: 'dark-square', piece: null },
+						{ square: 'd2', type: 'light-square', piece: null },
+						{ square: 'd1', type: 'dark-square', piece: null },
+					]
+				},
+				{
+					id: 'c',
+					ranks: [
+						{ square: 'c8', type: 'dark-square', piece: null },
+						{ square: 'c7', type: 'light-square', piece: null },
+						{ square: 'c6', type: 'dark-square', piece: null },
+						{ square: 'c5', type: 'light-square', piece: null },
+						{ square: 'c4', type: 'dark-square', piece: null },
+						{ square: 'c3', type: 'light-square', piece: null },
+						{ square: 'c2', type: 'dark-square', piece: null },
+						{ square: 'c1', type: 'light-square', piece: null },
+					]
+				},
+				{
+					id: 'b',
+					ranks: [
+						{ square: 'b8', type: 'light-square', piece: 'w_pawn' },
+						{ square: 'b7', type: 'dark-square', piece: 'w_pawn' },
+						{ square: 'b6', type: 'light-square', piece: 'w_pawn' },
+						{ square: 'b5', type: 'dark-square', piece: 'w_pawn' },
+						{ square: 'b4', type: 'light-square', piece: 'w_pawn' },
+						{ square: 'b3', type: 'dark-square', piece: 'w_pawn' },
+						{ square: 'b2', type: 'light-square', piece: 'w_pawn' },
+						{ square: 'b1', type: 'dark-square', piece: 'w_pawn' },
+					]
+				},
+				{
+					id: 'a',
+					ranks: [
+						{ square: 'a8', type: 'dark-square', piece: 'w_rook' },
+						{ square: 'a7', type: 'light-square', piece: 'w_knight' },
+						{ square: 'a6', type: 'dark-square', piece: 'w_bishop' },
+						{ square: 'a5', type: 'light-square', piece: 'w_queen' },
+						{ square: 'a4', type: 'dark-square', piece: 'w_king' },
+						{ square: 'a3', type: 'light-square', piece: 'w_bishop' },
+						{ square: 'a2', type: 'dark-square', piece: 'w_knight' },
+						{ square: 'a1', type: 'light-square', piece: 'w_rook' },
+					]
+				},
+			]
 		}
 	},
 	methods: {
-		loading() {
+		move(data) {
+			const piece = data.piece;
+			const square = data.square;
+			console.log(`You dropped: ${piece} on ${square}`);
 		}
+	},
+	mounted () {
+		this.board[7].ranks[7].piece = 'w_rook';
+		this.board_update = !this.board_update;
 	}
 }
 </script>
--- neochess-web/src/components/NeoPiece.vue
+++ neochess-web/src/components/NeoPiece.vue
@@ -1,7 +1,15 @@
 <template>
-<VueDraggableResizable 
-	class="piece"
-	:w="size" :h="size" @dragging="on_drag" :parent="false">
+<Drag 
+	class="drag piece"
+	:style="{
+		width: size + 'px', height: size + 'px', opacity
+	}"
+	:transfer-data="{ piece: id }"
+	effect-allowed="move"
+	:image-x-offset="size/2"
+	:image-y-offset="size/2"
+	@dragstart="move_started(id, position)"
+	@dragend="move_finished()" >
 	<img v-if="id === 'w_pawn'" class="piece-content" src="../assets/w_pawn.svg">
 	<img v-if="id === 'w_knight'" class="piece-content" src="../assets/w_knight.svg">
 	<img v-if="id === 'w_bishop'" class="piece-content" src="../assets/w_bishop.svg">
@@ -14,15 +22,31 @@
 	<img v-if="id === 'b_rook'" class="piece-content" src="../assets/b_rook.svg">
 	<img v-if="id === 'b_queen'" class="piece-content" src="../assets/b_queen.svg">
 	<img v-if="id === 'b_king'" class="piece-content" src="../assets/b_king.svg">
-</VueDraggableResizable>
+	<template slot="image">
+	<div class="drag" :style="{width: size + 'px', height: size + 'px'}">
+	<img v-if="id === 'w_pawn'" class="piece-content" src="../assets/w_pawn.svg">
+	<img v-if="id === 'w_knight'" class="piece-content" src="../assets/w_knight.svg">
+	<img v-if="id === 'w_bishop'" class="piece-content" src="../assets/w_bishop.svg">
+	<img v-if="id === 'w_rook'" class="piece-content" src="../assets/w_rook.svg">
+	<img v-if="id === 'w_queen'" class="piece-content" src="../assets/w_queen.svg">
+	<img v-if="id === 'w_king'" class="piece-content" src="../assets/w_king.svg">
+	<img v-if="id === 'b_pawn'" class="piece-content" src="../assets/b_pawn.svg">
+	<img v-if="id === 'b_knight'" class="piece-content" src="../assets/b_knight.svg">
+	<img v-if="id === 'b_bishop'" class="piece-content" src="../assets/b_bishop.svg">
+	<img v-if="id === 'b_rook'" class="piece-content" src="../assets/b_rook.svg">
+	<img v-if="id === 'b_queen'" class="piece-content" src="../assets/b_queen.svg">
+	<img v-if="id === 'b_king'" class="piece-content" src="../assets/b_king.svg">
+	</div>
+    </template>
+</Drag>
 </template>
 
 <script>
-import VueDraggableResizable from 'vue-draggable-resizable'
+import { Drag } from 'vue-drag-drop';
 export default {
 	name: "neochess-piece",
 	components: {
-		VueDraggableResizable
+		Drag
 	},
 	props: {
 		id: {
@@ -32,19 +56,28 @@ export default {
 		size: {
 			type: Number,
 			required: true,
+		},
+		start_position: {
+			type: String,
+			required: true,
 		}
 	},
 	data() {
 		return {
+			position: this.start_position,
+			opacity: 1
 		}
 	},
 	methods: {
 		update_size(new_size) {
 			this.size = new_size;
 		},
-		on_drag(x, y) {
-			this.x = x + 500;
-			this.y = y + 500;
+		move_started(id, position) {
+			console.log(`You are moving: ${id} from ${position}`);
+			this.opacity = 0.1
+		},
+		move_finished() {
+			this.opacity = 1
 		}
 	}
 }
