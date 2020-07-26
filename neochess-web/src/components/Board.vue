f82caf17d8a4f43a468ac818ce5b413802e93455
David Borges <davidborges@protonmail.com>
2020-07-26 18:39:02 -0300

    Trying out vue-chessboard

--- neochess-web/src/components/Board.vue
+++ neochess-web/src/components/Board.vue
@@ -1,33 +1,48 @@
 <template>
 <div id="neochess-board">
-	<chessboard :style="chessboard_style" id="board"/>
+	<chessboard ref="chessboard" :style="css_vars" id="board"/>
 </div>
 </template>
 
 <script>
-import {chessboard} from 'vue-chessboard'
+import {chessboard} from './vue-chessboard'
 export default {
 	name: "neochess-board",
 	components: {
 		chessboard
 	},
-	props: {},
+	props: {
+		size: {
+			type: String,
+			default: '700'
+		}
+	},
+	computed: {
+		css_vars() {
+			return {
+				'--size': this.size + 'px'
+			}
+		}
+	},
 	data() {
 		return {
-			game_id: this.$route.params.game_id,
+			game_id: this.$route.params.game_id
 		}
 	},
 	methods: {
 		loading() {
 		}
+	},
+	mounted() {
+		this.loading();
 	}
 }
 </script>
 
 <style>
 .cg-board-wrap {
-	width: 600px;
-	height: 600px;
+	width: var(--size);
+	height: var(--size);
 	margin: auto;
 }
 </style>
--- neochess-web/src/components/Game.vue
+++ neochess-web/src/components/Game.vue
@@ -2,30 +2,33 @@
 <div id="neochess-game">
 	<p class="neochess-title">game!</p>
 	<p>share this link with a friend to start playing: {{ game_url }}</p>
-	<NeoBoard size="700"/>
+	<Board :size="board_size"></Board>
 </div>
 </template>
 
 <script>
-import NeoBoard from "./NeoBoard.vue";
+import Board from "./Board.vue";
 export default {
 	name: "neochess-game",
 	components: {
-		NeoBoard
+		Board
 	},
 	props: {
-		url: String
 	},
 	data() {
 		return {
 			game_id: this.$route.params.game_id,
 			game_url: `${process.env.VUE_APP_URL}/#/game/${this.$route.params.game_id}`,
+			board_size: this.$route.params.board_size,
 		}
 	},
 	methods: {
 		loading() {
 		}
-	}
+	},
+	mounted() {
+		this.loading();
+	},
 }
 </script>
 
